ef9644f091b1a3dc02ca852973a645c3eff87d6f
Zeeshan Abid <zeesh.rocky@gmail.com>
2016-05-03 01:47:58 +0530

    ACD_JAVAB_Session_1_Assingment4
    Solving Expressions

---
+++ Assignment4/src/Expressions.java
@@ -0,0 +1,18 @@
+
+public class Expressions {
+	public static void main(String[] args) {
+		
+		int a,b,c,d;
+		int x=5,y=10;
+		
+		a=(x+y*2);
+		b=(x-y +2);
+		c=((x+y)*2);
+		d= (y%x);
+		
+		System.out.println("a= "+a);
+		System.out.println("b= "+b);
+		System.out.println("c= "+c);
+		System.out.println("d= "+d);
+	}
+}
